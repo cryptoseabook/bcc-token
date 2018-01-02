045f657b707b50524589f825a588dddc13bc9648
Seabook Chen <seabook1024@gmail.com>
2018-01-02 22:40:01 +1100

    init bcc token repo

---
+++ src/js/app.js
@@ -0,0 +1,105 @@
+App = {
+  web3Provider: null,
+  contracts: {},
+
+  init: function() {
+    return App.initWeb3();
+  },
+
+  initWeb3: function() {
+    // Initialize web3 and set the provider to the testRPC.
+    if (typeof web3 !== 'undefined') {
+      App.web3Provider = web3.currentProvider;
+      web3 = new Web3(web3.currentProvider);
+    } else {
+      // set the provider you want from Web3.providers
+      App.web3Provider = new Web3.providers.HttpProvider('http://127.0.0.1:9545');
+      web3 = new Web3(App.web3Provider);
+    }
+
+    return App.initContract();
+  },
+
+  initContract: function() {
+    $.getJSON('BCCToken.json', function(data) {
+      // Get the necessary contract artifact file and instantiate it with truffle-contract.
+      var bccTokenArtifact = data;
+      App.contracts.BCCToken = TruffleContract(bccTokenArtifact);
+
+      // Set the provider for our contract.
+      App.contracts.BCCToken.setProvider(App.web3Provider);
+
+      // Use our contract to retieve and mark the adopted pets.
+      return App.getBalances();
+    });
+
+    return App.bindEvents();
+  },
+
+  bindEvents: function() {
+    $(document).on('click', '#transferButton', App.handleTransfer);
+  },
+
+  handleTransfer: function(event) {
+    event.preventDefault();
+
+    var amount = parseFloat($('#BCCTokenTransferAmount').val());
+    var toAddress = $('#BCCTokenTransferAddress').val();
+
+    console.log('Transfer ' + amount + ' BCC Token to ' + toAddress);
+
+    var bccTokenInstance;
+
+    web3.eth.getAccounts(function(error, accounts) {
+      if (error) {
+        console.log(error);
+      }
+
+      var account = accounts[0];
+
+      App.contracts.BCCToken.deployed().then(function(instance) {
+        bccTokenInstance = instance;
+
+        return bccTokenInstance.transfer(toAddress, amount, {from: account});
+      }).then(function(result) {
+        alert('Transfer Successful!');
+        return App.getBalances();
+      }).catch(function(err) {
+        console.log(err.message);
+      });
+    });
+  },
+
+  getBalances: function(adopters, account) {
+    console.log('Getting balances...');
+
+    var bccTokenInstance;
+
+    web3.eth.getAccounts(function(error, accounts) {
+      if (error) {
+        console.log(error);
+      }
+
+      var account = accounts[0];
+
+      App.contracts.BCCToken.deployed().then(function(instance) {
+        bccTokenInstance = instance;
+
+        return bccTokenInstance.balanceOf(account);
+      }).then(function(result) {
+        balance = result.c[0];
+
+        $('#TTBalance').text(balance);
+      }).catch(function(err) {
+        console.log(err.message);
+      });
+    });
+  }
+
+};
+
+$(function() {
+  $(window).load(function() {
+    App.init();
+  });
+});
